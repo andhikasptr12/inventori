d446736009c92713a1f1354c43b70fe1057f252f
andhikasptr12 <adityagesang0@gmail.com>
2021-03-04 12:14:43 +0700

    five commit

--- app/Barang.php
+++ app/Barang.php
@@ -3,14 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Alfa6661\AutoNumber\AutoNumberTrait;
 
 class Barang extends Model
 {
+    use AutoNumbertrait;
     protected$table = 'barangs';
     protected$guarded = [];
+
+    public function getAutoNumberOptions()
+    {
+        return [
+            'kode_barang' => [
+                'format' => function() {
+                    return 'BRG/'.date('Ymd').'/?';
+                }, 'length' => 5]
+            ];
+    }
 
     public function suplier()
     {
         return $this->belongsTo(Suplier::class);
     }
+
+    public function barang()
+    {
+        return $this->belongsTo(Barang::class);
+    } 
 }
